bf6308676a573d1c43a923768791467adc147fba
Pedro Victor <pedrovictornt96@gmail.com>
2025-08-18 12:40:48 -0300

    feat: Atualizacao das requisicoes de cria, listar, listarporid e deletarporid das Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,33 +1,52 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
-import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("missoes")
+@RequestMapping("/missoes")
 public class MissoesController {
 
-    // GET -- Mandar uma requisicao para criar as missoes
-    @GetMapping("/listar")
-    public String listarMissoes() {
-        return "Missoes listadas com sucesso";
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
     }
 
     // POST -- Mandar uma requisicao para criar as missoes
+    // Adicionar missoes (CREATE)
     @PostMapping("/criar")
-    public String criarMissao(){
-        return "Missao criada com sucesso";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missoes) {
+        return missoesService.criarMissoes(missoes);
+    }
+
+    // GET -- Mandar uma requisicao para listar as missoes
+    // Mostrar todas as missoes (READ)
+    @GetMapping("/listar")
+    public List<MissoesModel> listarMissoes() {
+        return missoesService.listarMissoes();
+    }
+
+    // GET -- Manadar uma requisicao para listar por id as missoes
+    // Mostrar todas as missoes por id (READ)
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissoesPorId(@PathVariable Long id) {
+        return missoesService.listarMissoesPorId(id);
     }
 
     // PUT -- Mandar uma requisicao para alterar as missoes
+    // Alterar missoes (UPDATE)
     @PutMapping("/alterar")
     public String alterarMissao(){
         return "Missao alterada com sucesso";
     }
 
     // DELETE -- Mandar uma requisicao para deletar as missoes
-    @DeleteMapping("/deletar")
-    public String deletarMissao(){
-        return "Missao deletada com sucesso";
+    // Deletar missoes (DELETE)
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissaoPorId(@PathVariable Long id) {
+        missoesService.deletarMissoesPorId(id);
     }
 
 
