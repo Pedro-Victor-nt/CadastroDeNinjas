28cb8c1fe014274e20d866643e83adb94187a4d3
Pedro Victor <pedrovictornt96@gmail.com>
2025-09-08 14:01:41 -0300

    Refactor: Inclue a annotation ToString nos models de Ninja e Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 // Entity ele transforma uma classe em uma entidade do BD
 // JPA -> Java Persistence API
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missoes")
 public class NinjaModel {
 
     @Id
